99639e91ba7be7f9ec0d55ca9843f155eeacd354
Victor Rhea <160505305+rheanuvem@users.noreply.github.com>
2024-07-01 15:28:37 -0300

    Update couponRouter.js

--- couponRouter.js
+++ couponRouter.js
@@ -1,168 +1,183 @@
-const XLSX = require("xlsx");
+const fs = require("fs");
+const xlsx = require("xlsx");
+const path = require("path");
+const https = require("https");
+const { parse, format, isValid } = require("date-fns");
 
-// Configuration
-const STORE_ID = "3859035";
-const API_TOKEN = "a56594748f90fe9b2b2e407cf9351a5b002d0d01";
-const BASE_URL = `https://api.nuvemshop.com.br/v1/${STORE_ID}/coupons`;
+// Authentication and API Endpoint Configuration
 
-// Search string to be looked for in the "code"
-const SEARCHED_STRING = "TROCA";
+const storeId = "";
+const authenticationToken = "";
+const appId = "massCupom";
 
-// Starting page variable
-const startPage = 140; // Change this value to start from a different page
+// Source Directory for XLSX Files
+const sourceXlsxDir = path.join(__dirname, "sourceXLSX");
 
-// Filters
-const filter = {
-  id: "",
-  code: "",
+// Default values for coupon fields
+const defaultValues = {
+  code: "DEFAULT_CODE",
+  type: "absolute",
+  valid: true,
+  includes_shipping: true,
+  value: "0",
+  max_uses: 1,
+  min_price: 0,
+  categories: null,
+  start_date: () => format(new Date(), "yyyy-MM-dd"),
+  end_date: null,
+};
+
+// Headers Translation Object for Different XLSX Standards
+const headersTranslation = {
+  code: "Voucher",
   type: "",
-  value: "",
   valid: "",
-  used: "0",
+  value: "Value",
   max_uses: "",
-  include_shipping: "",
-  start_date: "",
-  end_date: "",
   min_price: "",
   categories: "",
+  start_date: "EmissionDate",
+  end_date: "ExpirationDate",
 };
 
-// Headers for API requests
-const headers = {
-  Authentication: `bearer ${API_TOKEN}`,
-  "User-Agent": "Your App Name (app)",
-  "Content-Type": "application/json",
+const typeTranslation = {
+  absolute: "fixed",
+  percent: "percent",
+  shipping: "shipping",
 };
 
-// Helper function to delay execution
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// Coupon Creation Limit for Testing
+const couponLimiter = 2000; // Set to 0 for no limit
 
-// Function to check if a coupon matches the filter and search string
-const matchesFilter = (coupon, filter, searchString) => {
-  if (searchString && !coupon.code.includes(searchString)) {
-    return false;
+// Date format from XLS files
+const inputDateFormat = "M/d/yy";
+
+function convertDate(sourceDate) {
+  if (!sourceDate) {
+    return null; // Return null if no date is provided for end_date
   }
-  for (const key in filter) {
-    if (filter[key] && coupon[key] != filter[key]) {
-      return false;
-    }
-  }
-  return true;
-};
 
-// Function to fetch a coupon by ID
-const fetchCouponById = async (couponId) => {
+  let parsedDate = parse(sourceDate, inputDateFormat, new Date());
+  if (isValid(parsedDate)) {
+    return format(parsedDate, "yyyy-MM-dd");
+  } else {
+    console.error("Invalid date format:", sourceDate);
+    return null;
+  }
+}
+
+function findXlsxFile(directory) {
+  const files = fs.readdirSync(directory);
+  const xlsxFile = files.find((file) => path.extname(file) === ".xlsx");
+  if (!xlsxFile) {
+    throw new Error("No XLSX file found in the directory.");
+  }
+  return path.join(directory, xlsxFile);
+}
+
+function readXlsxFile(filePath) {
+  const workbook = xlsx.readFile(filePath);
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+  const rawData = xlsx.utils.sheet_to_json(worksheet, { raw: false });
+  return rawData;
+}
+
+async function createCouponsForAllRows() {
   try {
-    const response = await fetch(`${BASE_URL}/${couponId}`, { headers });
+    const xlsxFilePath = findXlsxFile(sourceXlsxDir);
+    const rawData = readXlsxFile(xlsxFilePath);
+    const couponsToProcess = couponLimiter
+      ? rawData.slice(0, couponLimiter)
+      : rawData;
 
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch coupon with ID: ${couponId}, status: ${response.status}`
-      );
-    }
+    for (let i = 0; i < couponsToProcess.length; i++) {
+      await new Promise((resolve) => {
+        const couponData = couponsToProcess[i];
+        const couponObject = {
+          code: couponData[headersTranslation.code] || defaultValues.code,
+          type:
+            typeTranslation[
+              couponData[headersTranslation.type]?.toLowerCase()
+            ] || defaultValues.type,
+          valid:
+            (couponData[headersTranslation.valid] ||
+              String(defaultValues.valid)) === "true",
+          value: (couponData[headersTranslation.value] || defaultValues.value)
+            .replace("R$", "")
+            .trim()
+            .replace(",", "."),
+          max_uses: parseInt(
+            couponData[headersTranslation.max_uses] || defaultValues.max_uses,
+            10
+          ),
+          min_price: parseFloat(
+            couponData[headersTranslation.min_price] || defaultValues.min_price
+          ),
+          start_date: couponData[headersTranslation.start_date]
+            ? convertDate(couponData[headersTranslation.start_date])
+            : defaultValues.start_date(),
+          end_date: couponData[headersTranslation.end_date]
+            ? convertDate(couponData[headersTranslation.end_date])
+            : defaultValues.end_date,
+          includes_shipping:
+            (couponData[headersTranslation.shipping] ||
+              String(defaultValues.includes_shipping)) === "true",
+        };
 
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching coupon:", error.message);
-  }
-};
-
-// Function to edit a coupon
-const editCoupon = async (coupon) => {
-  try {
-    const updatedCoupon = {
-      code: coupon.code,
-      type: coupon.type,
-      value: coupon.value,
-      valid: coupon.valid,
-      used: coupon.used,
-      max_uses: coupon.max_uses,
-      start_date: coupon.start_date,
-      end_date: coupon.end_date,
-      include_shipping: true,
-    };
-
-    const response = await fetch(`${BASE_URL}/${coupon.id}`, {
-      method: "PUT",
-      headers,
-      body: JSON.stringify(updatedCoupon),
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Failed to edit coupon with ID: ${coupon.id}, status: ${response.status}`
-      );
-    }
-
-    const responseBody = await response.json();
-    console.log("Coupon edited successfully:", responseBody);
-  } catch (error) {
-    console.error("Error editing coupon:", error.message);
-  }
-};
-
-// Array to store log data for the Excel file
-let logData = [["Coupon ID", "Code", "Match Status"]];
-
-const fetchCoupons = async (page = startPage) => {
-  try {
-    const response = await fetch(`${BASE_URL}?page=${page}`, { headers });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const coupons = await response.json();
-    const totalCoupons = parseInt(response.headers.get("x-total-count"), 10);
-    const currentCouponsCount = coupons.length;
-
-    // Log page number
-    console.log(`\n---------------------------------------------------`);
-    console.log(`Coupons on Page ${page}`);
-    console.log(`---------------------------------------------------\n`);
-
-    for (const coupon of coupons) {
-      let matchStatus = "❌"; // Default status if no match
-
-      // Check if the coupon matches the filter criteria
-      if (matchesFilter(coupon, filter, SEARCHED_STRING)) {
-        matchStatus = "✅"; // Set status to green checkmark if match
         console.log(
-          `✅ Coupon ID: ${coupon.id}, Code: ${coupon.code} matches the filter and contains the string ${SEARCHED_STRING} in the code`
+          "Generated coupon object:",
+          JSON.stringify(couponObject, null, 2)
         );
-        // Add matching coupon data to log array for Excel file
-        logData.push([coupon.id, coupon.code, matchStatus]);
-
-        // Fetch the complete coupon data
-        const fullCoupon = await fetchCouponById(coupon.id);
-
-        // Execute the function to edit the matched coupon
-        await editCoupon(fullCoupon);
-      } else {
-        console.log(`❌ Coupon ID: ${coupon.id}, Code: ${coupon.code}`);
-      }
-
-      // Delay to avoid 429 errors
-      await delay(300);
-    }
-
-    // If there are more coupons to fetch, recursively fetch the next page
-    if (
-      currentCouponsCount + (page - startPage) * currentCouponsCount <
-      totalCoupons
-    ) {
-      await fetchCoupons(page + 1);
-    } else {
-      // Once all pages are fetched, write the data to an Excel file
-      const workbook = XLSX.utils.book_new();
-      const worksheet = XLSX.utils.aoa_to_sheet(logData);
-      XLSX.utils.book_append_sheet(workbook, worksheet, "Coupons");
-      XLSX.writeFile(workbook, "coupons_log.xlsx");
-      console.log("\n✅ Coupons log has been written to coupons_log.xlsx");
+        createCoupon(couponObject, () => {
+          console.log(`Coupon for ${couponObject.code} created.`);
+          setTimeout(resolve, 700); // 700 milliseconds delay
+        });
+      });
     }
   } catch (error) {
-    console.error("Error fetching coupons:", error.message);
+    console.error("Error processing coupons:", error);
   }
-};
+}
 
-fetchCoupons();
+function createCoupon(couponObject, callback) {
+  const options = {
+    hostname: "api.nuvemshop.com.br",
+    path: `/v1/${storeId}/coupons`,
+    method: "POST",
+    headers: {
+      Authentication: `bearer ${authenticationToken}`,
+      "User-Agent": appId,
+      "Content-Type": "application/json",
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    let responseBody = "";
+    res.on("data", (chunk) => {
+      responseBody += chunk;
+    });
+    res.on("end", () => {
+      console.log(
+        `Response for coupon ${couponObject.code}: Status ${res.statusCode}`
+      );
+      try {
+        const response = JSON.parse(responseBody);
+        console.log(response);
+      } catch (error) {
+        console.error("Error parsing response:", error);
+      }
+      if (callback) callback();
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error(`Request error for coupon ${couponObject.code}:`, error);
+    if (callback) callback(error);
+  });
+
+  req.write(JSON.stringify(couponObject));
+  req.end();
+}
+
+createCouponsForAllRows();
